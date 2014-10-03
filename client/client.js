65682e7e9a9f3ae9858f628e1eb2de8a5d504126
david spanel <david@spanel.name>
2014-10-03 14:30:23 +0200

    Saving file name

--- client/client.js
+++ client/client.js
@@ -2,9 +2,19 @@ Meteor.subscribe("trips");
 
 Template.newtrip.events({
 'click .newtrip' : function() {Session.set("ShowTripForm", true) ;console.log("blablabla")},
-'click .cancel.trip' : function() {Session.set("ShowTripForm", false)}
+'click .cancel.trip' : function() {Session.set("ShowTripForm", false)},
+'click .save.trip' : function() {saveTrip()}
 });
 
+function saveTrip() {
+	console.log(Trips.insert({
+		tripname: $("#tripname")[0].value,
+		datetime: $("#tripdatetime")[0].value,
+		destination: "",
+		cars: {},
+		people: {}
+	}))
+}
 Template.newtrip.ShowTripForm = function () { return Session.get("ShowTripForm") };
 
 Template.tripform.rendered = function () {
